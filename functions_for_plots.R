e2ac9ebdab63a9f6830e84f512f8218f6f7b4501
Lizzie Pearmain <ejp69@cam.ac.uk>
2023-04-27 11:49:05 +0100

    started on plotting functions

---
+++ functions_for_plots.R
@@ -0,0 +1,8 @@
+
+## Functions for plotting CCI electricity data
+
+## Here we can add new functions for plotting data, once we have 
+##   developed and finalised them in s02_plots.Rmd.
+## See https://r4ds.had.co.nz/functions.html for help on writing functions.
+
+
